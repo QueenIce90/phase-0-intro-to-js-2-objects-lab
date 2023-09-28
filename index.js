0204a34dfb67d45d0f66e715297b7ce62a2d3271
QueenIce90 <Isamargonzalez90@gmail.com>
2023-09-28 19:00:42 -0400

    short messagebriefly explaining the changes saved in this commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+// Define the employee object
+const employee = {
+  name: "John Doe",
+  streetAddress: "123 Main Street",
+};
+
+// Function to update employee non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+// Function to update employee destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function to delete key non-destructively
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function to delete key destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+module.exports = {
+  updateEmployeeWithKeyAndValue,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyDeleteFromEmployeeByKey,
+};
+
+
+  
